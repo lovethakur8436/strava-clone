44daff1667fb5f181d345d04fa3960de7394debb
luv18031 <luv.cs.18031@recb.ac.in>
2026-08-30 13:58:05 +0530

    working version of activity edit and delete

--- src/main/java/com/fitness/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fitness/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.fitness.common.exception;
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
